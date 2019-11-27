46544c397cb6c4fb924e39bc1be25ac30a8111a3
twopheat <twopheat@gmail.com>
2019-11-26 16:11:53 -0800

    starting openweather api

--- assets/js/script.js
+++ assets/js/script.js
@@ -8,3 +8,35 @@ $(document).ready(function() {
       }
     });
   });
+
+
+  $("#search-button").on("click", function() {
+
+    // Storing our Open Weather API URL for query
+    var queryURL = "https://api.openweathermap.org/data/2.5/forecast?id=524901&APPID=bbea62cdba22465b7f7589c50dd2081e";
+    
+
+    // Perfoming an AJAX GET request to our queryURL
+    $.ajax({
+      url: queryURL,
+      method: "GET"
+    })
+    console.log(queryURL);
+    // After the data from the AJAX request comes back
+      .then(function(response) {
+
+      // Saving the image_original_url property
+        var wData = response.data.list;
+        var weatherData = $("<today-weather>");
+
+        // Setting the weatherData src attribute to imageUrl
+        weatherData.attr("list.main.temp", wData);
+        weatherData.attr("list.wind.speed", "cat image");
+
+        // Prepending the weatherData to the images div
+        $("#").prepend(weatherData);
+      });
+  });
+  
+
+  
